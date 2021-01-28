b6d45a2b112ac25e2ce6e042a0678e26ec39092c
Barrett879 <77691102+Barrett879@users.noreply.github.com>
2021-01-28 10:10:29 -0800

    Update myscript.js

--- myscript.js
+++ myscript.js
@@ -1,8 +1,9 @@
 $(document).ready(function() {
-	$("h1").click(function() {
-		$("p").css({"color":"red", "background-color": "yellow"});
-		$("img").css("border","25px solid black");
-	});
+	  
+$("#p1").click(function(){
+  $(".disappear").toggle();
+});
+  
 	$("#imagetoggler").click(function(){
 		$("img").toggle();
 	});
